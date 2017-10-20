32309994fc883607e3057ffe74f7028588001365
cucy <292016176@qq.com>
2017-10-20 18:56:49 +0800

    类装饰器

---
+++ python/装饰器/5_类装饰器.py
@@ -0,0 +1,35 @@
+class Foo:
+    def __call__(self, *args, **kwargs):
+        print('call....')
+
+    def test(self):  #
+        print('test....')
+
+
+t = Foo()  # 实例化类
+t.test()  # 正常调用实例方法
+t()  # 直接调用实例化之后的对象
+
+print('\n--------------------\n')
+class Fuck(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        import time
+        start_time = time.time()
+        res = self.func(*args, **kwargs)
+        end_time = time.time()
+        print('the function "%s" run time is %s' % (self.func.__name__,
+                                                    (end_time - start_time)))
+        return res
+
+
+@Fuck
+def run(name):
+    import time
+    time.sleep(1)
+    return 'sb_%s' % name
+
+
+print(run('hyf'))
