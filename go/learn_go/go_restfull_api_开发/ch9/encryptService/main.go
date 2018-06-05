951af8a3ef97b7327cc3a32fcf974c2c6d14d398
cucy <292016176@qq.com>
2018-06-05 18:41:40 +0800

    '添加日志服务'

--- go/learn_go/go_restfull_api_开发/ch9/encryptService/main.go
+++ go/learn_go/go_restfull_api_开发/ch9/encryptService/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	kitlog "github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/narenaryan/encryptService/helpers"
 )
 
 func main() {
-	svc := helpers.EncryptServiceInstance{}
+	logger := kitlog.NewLogfmtLogger(os.Stderr)
+	var svc helpers.EncryptService
+	svc = helpers.EncryptServiceInstance{}
+	svc = helpers.LoggingMiddleware{Logger: logger, Next: svc}
 	encryptHandler := httptransport.NewServer(helpers.MakeEncryptEndpoint(svc),
 		helpers.DecodeEncryptRequest,
 		helpers.EncodeResponse)
