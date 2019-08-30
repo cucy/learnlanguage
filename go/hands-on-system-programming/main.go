be1458fa84f5912770bbf79c27aef0649d5e9fd6
zrd <292016176@qq.com>
2019-08-30 11:34:50 +0800

    go 总结

--- go/hands-on-system-programming/main.go
+++ go/hands-on-system-programming/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net/http"
 	"time"
 )
 
 func main() {
-	var a int // passing value
-	go func(v int) { fmt.Println(v) }(a)
-	// passing pointer
-	go func(v *int) { fmt.Println(*v) }(&a)
-	a = 42
-	time.Sleep(time.Nanosecond)
+	const addr = "localhost:8080"
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(5 * time.Second)
+	})
+
+	go func() {
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			panic(err)
+		}
+	}()
+
+	req, _ := http.NewRequest(http.MethodGet, "http://"+addr, nil)
+
+	ctx, canc := context.WithTimeout(context.Background(), time.Second*2)
+	defer canc()
+
+	time.Sleep(time.Second)
+	if _, err := http.DefaultClient.Do(req.WithContext(ctx)); err != nil {
+		log.Fatal("超时", err)
+	}
 }
+
+// 2019/08/28 09:28:28 超时Get http://localhost:8080: context deadline exceeded
